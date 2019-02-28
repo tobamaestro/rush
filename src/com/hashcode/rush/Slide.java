0ea1d3568783e2c6d9daee22441eea5bb18150ce
Aleksandar Andjelkovic <aleksandar.andjelkovic@realmarket.io>
2019-02-28 21:48:06 +0100

    Added transitions

--- src/com/hashcode/rush/Slide.java
+++ src/com/hashcode/rush/Slide.java
@@ -1,13 +1,18 @@
 package com.hashcode.rush;
 
 import java.util.List;
+import java.util.Map;
 
 public class Slide {
 
     private List<Photo> photos;
+    private Integer distance = Integer.MAX_VALUE;
+    private Map<Slide, Integer> transitions;
 
-    public Slide(List<Photo> photos) {
+    public Slide(List<Photo> photos, Integer distance, Map<Slide, Integer> transitions) {
         this.photos = photos;
+        this.distance = distance;
+        this.transitions = transitions;
     }
 
     public List<Photo> getPhotos() {
@@ -16,5 +21,21 @@ public class Slide {
 
     public void setPhotos(List<Photo> photos) {
         this.photos = photos;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Map<Slide, Integer> getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(Map<Slide, Integer> transitions) {
+        this.transitions = transitions;
     }
 }
